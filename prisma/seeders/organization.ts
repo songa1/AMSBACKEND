5ee691a8902be828a9719804ef751bdd98d670d1
songa1 <songachillethe1her@gmail.com>
2024-09-08 17:54:40 +0200

    I have added working sectors seeders

--- prisma/seeders/organization.ts
+++ prisma/seeders/organization.ts
@@ -11,13 +11,123 @@ const organizations = [
 
 const workingSectors = [
   {
-    id: "education",
+    id: "climate",
+    name: "Climate Change",
+    createdAt: new Date(),
+  },
+  {
+    id: "communication",
+    name: "Media & Communication",
+    createdAt: new Date(),
+  },
+  {
+    id: "civic",
+    name: "Civic Engagement & Volunteerism",
+    createdAt: new Date(),
+  },
+  {
+    id: "sport",
+    name: "Sport & Recreation",
+    createdAt: new Date(),
+  },
+  {
+    id: "agri",
+    name: "Agiculture Development",
+    createdAt: new Date(),
+  },
+  {
+    id: "fin",
+    name: "Financial Literacy",
+    createdAt: new Date(),
+  },
+  {
+    id: "eship",
+    name: "Entrepreneurship & Small Business Support",
+    createdAt: new Date(),
+  },
+  {
+    id: "sexed",
+    name: "Sexual Health Education",
+    createdAt: new Date(),
+  },
+  {
+    id: "tourism",
+    name: "Tourism",
+    createdAt: new Date(),
+  },
+  {
+    id: "waste",
+    name: "Waste Management & Recycling",
+    createdAt: new Date(),
+  },
+  {
+    id: "bustrade",
+    name: "Business & Trade",
+    createdAt: new Date(),
+  },
+  {
+    id: "health",
+    name: "Healthcare & Mental Wellbeing",
+    createdAt: new Date(),
+  },
+  {
+    id: "ed",
     name: "Education",
     createdAt: new Date(),
   },
   {
-    id: "ict",
-    name: "ICT",
+    id: "env",
+    name: "Environmental Conservation",
+    createdAt: new Date(),
+  },
+  {
+    id: "human",
+    name: "Human Rights Advocates",
+    createdAt: new Date(),
+  },
+  {
+    id: "economy",
+    name: "Economic Development",
+    createdAt: new Date(),
+  },
+  {
+    id: "art",
+    name: "Art & Culture",
+    createdAt: new Date(),
+  },
+  {
+    id: "community",
+    name: "Community Development",
+    createdAt: new Date(),
+  },
+  {
+    id: "gender",
+    name: "Gender Equality",
+    createdAt: new Date(),
+  },
+  {
+    id: "tech",
+    name: "Technology & Digital Inclusion",
+    createdAt: new Date(),
+  },
+  {
+    id: "eng",
+    name: "Engineering",
+    createdAt: new Date(),
+  },
+  {
+    id: "youth",
+    name: "Youth & Child Development",
+    createdAt: new Date(),
+  },
+  {
+    id: "governance",
+    name: "Governmance",
+    createdAt: new Date(),
+  },
+  {
+    id: "water",
+    name: "Water & Sanitation",
     createdAt: new Date(),
   },
   {
