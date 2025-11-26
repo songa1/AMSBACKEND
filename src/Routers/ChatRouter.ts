5155db071b1b4fed57c1d4ad199c54a2fbc8f2b7
songa1 <songachillethe1her@gmail.com>
2025-11-26 02:57:00 +0200

    Made a lot of changes on the users

--- src/Routers/ChatRouter.ts
+++ src/Routers/ChatRouter.ts
@@ -5,9 +5,11 @@ import {
   getPrivateMessages,
   sendMessage,
 } from "../controllers/ChatControllers/MessageController";
+import { getTopActiveMembers } from "../controllers/ChatControllers/topMembers";
 
 const chatRouter = Router();
 
+chatRouter.get("/top", getTopActiveMembers);
 chatRouter.post("/", sendMessage);
 chatRouter.get("/", getMessages);
 chatRouter.get("/:id/:oid", getPrivateMessages);
